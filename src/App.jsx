38370c87f2e42c1f1339e769b98a352afb60687a
cat-ninja007 <150651906+cat-ninja007@users.noreply.github.com>
2025-10-29 16:08:42 +0800

    feat: adding routes navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import Navigation from "./components/Navigation";
+import HomePage from "./pages/HomePage";
+import AddNotePage from "./pages/AddNotePage";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return (
-    <div className="app-container">
-      <h1>Hello, React</h1>
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<header>
+				<h1>Personal Notes</h1>
+				<Navigation />
+			</header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AddNotePage />} />
+        </Routes>
+      </main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/AddNotePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddNotePage(){
+  return(
+    <>
+      <h1>This is Add Note Page</h1>
+    </>
+  )
+}
+
+export default AddNotePage
